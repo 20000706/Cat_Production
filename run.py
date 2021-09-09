e9c6fe00ca7d318e336c418e00c495fc99db2886
20000706 <67904518+20000706@users.noreply.github.com>
2021-09-09 14:23:25 +1200

    update for handing in

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -11,13 +11,23 @@ def index():
 
 @app.route('/coatcolor')
 def coatcolor():
+    args = request.args
+    catType = args['type']
     db = sqlite3.connect(CATDB)
     coatcolors = []
-    cur = db.execute('SELECT coatcolor,texture,model FROM coatcolors')
+    if catType == 'S' or catType == 'BW' or catType == 'MT':
+        cur = db.execute(f"SELECT coatcolor,texture,model FROM coatcolors WHERE type = '{catType}'")
+    else:
+        cur = db.execute('SELECT coatcolor,texture,model FROM coatcolors')
     for row in cur:
         coatcolors.append(list(row))
     db.close()
-    return render_template('coatcolor.html', coatcolors=coatcolors)
+    if catType == 'BW':
+        return render_template('coatcolor.html', coatcolors=coatcolors, model='cat_body_BW.glb')
+    elif catType == 'MT':
+        return render_template('coatcolor.html', coatcolors=coatcolors, model='cat_body_MT.glb')
+    else:
+        return render_template('coatcolor.html', coatcolors=coatcolors, model='cat_body_S.glb')
 
 @app.route('/eyecolor')
 def eyecolor():
